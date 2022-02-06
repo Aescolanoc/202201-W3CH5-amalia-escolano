22a00201eb2650ea01a1353c61204f59ef8252d0
Mel Escolano <amalia.escolano@gmail.com>
2022-02-06 11:41:09 +0100

    Create favotites html and js files

--- public/services/api.js
+++ public/services/api.js
@@ -1,3 +1,7 @@
 export function getPokemons(url) {
   return fetch(url).then((resp) => resp.json());
 }
+
+export function getFavorites() {
+  return fetch("http://localhost:3000/Pokemon").then((resp) => resp.json());
+}
